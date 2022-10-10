92811c1c105d697d5a21d61aaff9e90b6efe665a
Allan Jardine <allan@datatables.net>
2022-10-10 19:49:49 +0000

    Add support for mjs files (ES Modules)

---
+++ js/keyTable.jqueryui.mjs
@@ -0,0 +1,13 @@
+
+/*! jQuery UI styling wrapper for KeyTable
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-ju';
+import DataTable from 'datatables.net-keytable';
+
+
+
+
+export default DataTable;
