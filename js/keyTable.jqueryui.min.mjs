bae20425ffb48379b88113be704a9612f2a1de32
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 11:52:06 +0000

    2de7c4a50f823d633cecea052206234aa575c1ba
    Sync to source repo @2de7c4a50f823d633cecea052206234aa575c1ba

--- js/keyTable.jqueryui.min.mjs
+++ js/keyTable.jqueryui.min.mjs
@@ -1,1 +1,4 @@
+/*! jQuery UI styling wrapper for KeyTable
+ * © SpryMedia Ltd - datatables.net/license
+ */
 import $ from"jquery";import DataTable from"datatables.net-ju";import DataTable from"datatables.net-keytable";export default DataTable;
