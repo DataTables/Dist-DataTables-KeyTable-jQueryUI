ff32927939718acd338fea07ba389b34609790bd
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-16 17:03:43 +0000

    efe0d4e9e38c5d8139abbb42086bb0cc5750b1ab
    Sync to source repo @fe0d4e9e38c5d8139abbb42086bb0cc5750b1ab

--- js/keyTable.jqueryui.js
+++ js/keyTable.jqueryui.js
@@ -5,7 +5,7 @@
 (function( factory ){
 	if ( typeof define === 'function' && define.amd ) {
 		// AMD
-		define( ['jquery', 'datatables.net-ju', 'datatables.net-keytable'], function ( $ ) {
+		define( ['jquery', 'datatables.net-jqui', 'datatables.net-keytable'], function ( $ ) {
 			return factory( $, window, document );
 		} );
 	}
@@ -25,13 +25,12 @@
 			}
 
 			if ( ! $.fn.dataTable ) {
-				require('datatables.net-ju')(root, $);
+				require('datatables.net-jqui')(root, $);
 			}
 
-			if ( ! $.fn.dataTable ) {
+			if ( ! $.fn.dataTable.KeyTable ) {
 				require('datatables.net-keytable')(root, $);
 			}
-
 
 			return factory( $, root, root.document );
 		};
--- js/keyTable.jqueryui.min.mjs
+++ js/keyTable.jqueryui.min.mjs
@@ -1,4 +1,4 @@
 /*! jQuery UI styling wrapper for KeyTable
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-ju";import"datatables.net-keytable";export default DataTable;
+import $ from"jquery";import DataTable from"datatables.net-jqui";import KeyTable from"datatables.net-keytable";export default DataTable;
